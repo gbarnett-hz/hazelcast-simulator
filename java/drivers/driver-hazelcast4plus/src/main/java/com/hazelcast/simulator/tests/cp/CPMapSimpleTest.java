f23b03954dab2960eaea07bf232bbdf875f51bb4
Granville Barnett <granville.barnett@hazelcast.com>
2025-10-01 17:55:43 +0100

    executor for initialisation

--- java/drivers/driver-hazelcast4plus/src/main/java/com/hazelcast/simulator/tests/cp/CPMapSimpleTest.java
+++ java/drivers/driver-hazelcast4plus/src/main/java/com/hazelcast/simulator/tests/cp/CPMapSimpleTest.java
@@ -27,99 +27,134 @@ import com.hazelcast.simulator.test.annotations.Verify;
 import com.hazelcast.simulator.tests.cp.helpers.CpMapOperationCounter;
 import com.hazelcast.simulator.utils.GeneratorUtils;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 import static org.junit.Assert.assertTrue;
 
 public class CPMapSimpleTest extends HazelcastTest {
-  public int keys = 1;
-  // number of possible values
-  public int valueCount = 100;
-  // size in bytes for each key's associated value
-  public int valueSizeBytes = 100;
-  // name of map
-  public String mapName;
+	public int keys = 1;
+	// number of possible values
+	public int valueCount = 100;
+	// size in bytes for each key's associated value
+	public int valueSizeBytes = 100;
+	// name of map
+	public String mapName;
 
-  private byte[][] values;
+	private byte[][] values;
 
-  private CPMap<Long, byte[]> map;
+	private CPMap<Long, byte[]> map;
 
-  private IList<CpMapOperationCounter> operationCounterList;
+	private IList<CpMapOperationCounter> operationCounterList;
 
-  @Setup
-  public void setup() {
-    values = createValues();
-    map = targetInstance.getCPSubsystem().getMap(mapName);
-    // all clients (and threads) dump their stats into this list
-    operationCounterList = targetInstance.getList(name + "-report");
-  }
+	@Setup
+	public void setup() {
+		values = createValues();
+		map = targetInstance.getCPSubsystem().getMap(mapName);
+		// all clients (and threads) dump their stats into this list
+		operationCounterList = targetInstance.getList(name + "-report");
+	}
 
-  // populates the key space with a random value from values
-  // before we begin
-  @Prepare(global = true)
-  public void prepare() {
-    Random random = new Random(0);
-    for (long i = 0; i < keys; i++) {
-      map.set(i, values[random.nextInt(values.length)]);
-    }
-  }
+	// populates the key space with a random value from values
+	// before we begin
+	@Prepare(global = true)
+	public void prepare() {
+		int threadCount = Runtime.getRuntime().availableProcessors();
+		ExecutorService executor = Executors.newFixedThreadPool(threadCount);
+		Random random = new Random(0);
+		List<Future<?>> futures = new ArrayList<>(keys);
+		try {
+			for (long key = 0; key < keys; key++) {
+				final long mapKey = key;
+				final byte[] value = values[random.nextInt(values.length)];
+				futures.add(executor.submit(() -> map.set(mapKey, value)));
+			}
+			for (Future<?> future : futures) {
+				try {
+					future.get();
+				} catch (ExecutionException e) {
+					throw new RuntimeException("Failed to populate CPMap during prepare",
+							e.getCause());
+				}
+			}
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+			throw new RuntimeException("Prepare interrupted while populating CPMap", e);
+		} finally {
+			executor.shutdown();
+			try {
+				if (!executor.awaitTermination(1, TimeUnit.MINUTES)) {
+					executor.shutdownNow();
+				}
+			} catch (InterruptedException e) {
+				executor.shutdownNow();
+				Thread.currentThread().interrupt();
+			}
+		}
+	}
 
-  private byte[][] createValues() {
-    byte[][] valuesArray = new byte[valueCount][valueSizeBytes];
-    Random random = new Random(0);
-    for (int i = 0; i < valuesArray.length; i++) {
-      valuesArray[i] = GeneratorUtils.generateByteArray(random, valueSizeBytes);
-    }
-    return valuesArray;
-  }
+	private byte[][] createValues() {
+		byte[][] valuesArray = new byte[valueCount][valueSizeBytes];
+		Random random = new Random(0);
+		for (int i = 0; i < valuesArray.length; i++) {
+			valuesArray[i] = GeneratorUtils.generateByteArray(random, valueSizeBytes);
+		}
+		return valuesArray;
+	}
 
-  @TimeStep(prob = 1)
-  public void put(ThreadState state) {
-    map.put(state.randomKey(), state.randomValue());
-    state.operationCounter.putCount++;
-  }
+	@TimeStep(prob = 1)
+	public void put(ThreadState state) {
+		map.put(state.randomKey(), state.randomValue());
+		state.operationCounter.putCount++;
+	}
 
-  @TimeStep(prob = 0)
-  public void get(ThreadState state) {
-    map.get(state.randomKey());
-    state.operationCounter.getCount++;
-  }
+	@TimeStep(prob = 0)
+	public void get(ThreadState state) {
+		map.get(state.randomKey());
+		state.operationCounter.getCount++;
+	}
 
-  @AfterRun
-  public void afterRun(ThreadState state) {
-    operationCounterList.add(state.operationCounter);
-  }
+	@AfterRun
+	public void afterRun(ThreadState state) {
+		operationCounterList.add(state.operationCounter);
+	}
 
-  @Verify(global = true)
-  public void verify() {
-    // summation of all stats, all threads, all clients
-    CpMapOperationCounter total = new CpMapOperationCounter();
-    for (CpMapOperationCounter operationCounter : operationCounterList) {
-      total.add(operationCounter);
-    }
-    logger.info(name + ": " + total + " from " + operationCounterList.size() + " worker threads");
+	@Verify(global = true)
+	public void verify() {
+		// summation of all stats, all threads, all clients
+		CpMapOperationCounter total = new CpMapOperationCounter();
+		for (CpMapOperationCounter operationCounter : operationCounterList) {
+			total.add(operationCounter);
+		}
+		logger.info(name + ": " + total + " from " + operationCounterList.size() + " worker threads");
 
-    // basic verification
-    int entriesCount = 0;
-    for (long key = 0; key < keys; key++) {
-      byte[] v = map.get(key);
-      if (v != null) {
-        entriesCount++;
-      }
-    }
+		// basic verification
+		int entriesCount = 0;
+		for (long key = 0; key < keys; key++) {
+			byte[] v = map.get(key);
+			if (v != null) {
+				entriesCount++;
+			}
+		}
 
-    logger.info(name + ":  CP Map " + mapName + " entries count: " + entriesCount);
-    assertTrue("CP Map " + map.getName() + " doesn't contain any of expected items.", entriesCount > 0);
-  }
+		logger.info(name + ":  CP Map " + mapName + " entries count: " + entriesCount);
+		assertTrue("CP Map " + map.getName() + " doesn't contain any of expected items.", entriesCount > 0);
+	}
 
-  public class ThreadState extends BaseThreadState {
-    final CpMapOperationCounter operationCounter = new CpMapOperationCounter();
+	public class ThreadState extends BaseThreadState {
+		final CpMapOperationCounter operationCounter = new CpMapOperationCounter();
 
-    public long randomKey() {
-      return randomLong(keys);
-    }
+		public long randomKey() {
+			return randomLong(keys);
+		}
 
-    public byte[] randomValue() {
-      return values[randomInt(valueCount)]; // [0, values)
-    }
-  }
+		public byte[] randomValue() {
+			return values[randomInt(valueCount)]; // [0, values)
+		}
+	}
 }
